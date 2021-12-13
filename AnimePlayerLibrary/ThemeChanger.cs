5a21b8b71910c93f6f8871b787cb362fc02eb773
Kiko kiko <ssasske08@gmail.com>
2021-12-13 20:32:29 +0100

    Update

---
+++ AnimePlayerLibrary/ThemeChanger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AnimePlayerLibrary
+{
+    public static class ThemeChanger
+    {
+        public static void SetDefaultBackgroundColorFLP(Panel panel)
+        {
+            if(panel == null)
+            {
+                return;
+            }
+            panel.BackColor = Color.FromArgb(20, 20, 20);
+        }
+
+        public static void InterpretingFile(Form form)
+        {
+            try
+            {
+                if(form == null)
+                {
+                    return ;
+                }
+                Panel panelStartPage = (Panel)form.Controls.Find("panelStartPage", true)[0];
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
